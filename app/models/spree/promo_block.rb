feb82853c0747d5513f8ae8236ff681bb54bab2c
Edwin Horneij <edwin@astekwallcovering.com>
2016-10-03 15:01:21 -0700

    Require page URL to be unique for active promo blocks only, not globally

--- app/models/spree/promo_block.rb
+++ app/models/spree/promo_block.rb
@@ -3,5 +3,8 @@ module Spree
     self.table_name = 'promo_blocks'
     has_many :promo_block_items
     accepts_nested_attributes_for :promo_block_items
+
+    validates_presence_of :name, :active, :page_url
+    validates_uniqueness_of :page_url, conditions: -> { where(active: true) }
   end
 end
