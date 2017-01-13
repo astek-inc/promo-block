332308dcad1e3e9a91eb150b87ea75fd214b1448
joshlikesyou <joshlikesyou@gmail.com>
2017-01-13 15:10:33 -0800

    added compression to promo blocks images

--- app/models/spree/promo_block_item.rb
+++ app/models/spree/promo_block_item.rb
@@ -7,7 +7,9 @@ module Spree
 
     belongs_to :promo_block
 
-    has_attached_file :image, styles: {large: '800x760!', medium: '475x780!', small: '800x460!'}
+    has_attached_file :image,
+      styles: {large: '800x760!', medium: '475x780!', small: '800x460!'},
+      convert_options: { all: '-colorspace sRGB -quality 70'}
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
     default_scope {order('position ASC')}
