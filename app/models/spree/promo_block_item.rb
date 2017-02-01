76000ccb96378d5c24a19797453121157c891800
joshlikesyou <joshlikesyou@gmail.com>
2017-01-31 16:56:27 -0800

    removed image configurations

--- app/models/spree/promo_block_item.rb
+++ app/models/spree/promo_block_item.rb
@@ -8,8 +8,7 @@ module Spree
     belongs_to :promo_block
 
     has_attached_file :image,
-      styles: {large: '800x760!', medium: '475x780!', small: '800x460!'},
-      convert_options: { all: '-colorspace sRGB -quality 70'}
+      styles: {large: '800x760!', medium: '475x780!', small: '800x460!'}
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
     default_scope {order('position ASC')}
