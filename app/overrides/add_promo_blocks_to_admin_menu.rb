e89ed750b670ae03c133e44409600e9bcd656b24
Edwin Horneij <edwin@astekwallcovering.com>
2017-02-21 14:40:00 -0800

    Update original for override

--- app/overrides/add_promo_blocks_to_admin_menu.rb
+++ app/overrides/add_promo_blocks_to_admin_menu.rb
@@ -1,9 +1,9 @@
 Deface::Override.new(
-    :original => 'a2e58f4c6e226d173c5dc642aea458728f54bff6',
-    :virtual_path => 'spree/layouts/admin',
-    :name => 'add_promo_blocks_item_to_admin_menu',
-    :insert_bottom => '[data-hook="admin_tabs"]',
-    :text => "<ul class=\"nav nav-sidebar\">
-              <%= tab Spree.t(:promo_blocks), :url => admin_promo_blocks_path, :icon => 'stop' %>
+    original: '0557bcc4171004928c8eade01ba90d34a67b1a3e',
+    virtual_path: 'spree/layouts/admin',
+    name: 'add_promo_blocks_item_to_admin_menu',
+    insert_bottom: '[data-hook="admin_tabs"]',
+    text: "<ul class=\"nav nav-sidebar\">
+              <%= tab Spree.t(:promo_blocks), url: admin_promo_blocks_path, icon: 'stop' %>
           </ul>"
 )
